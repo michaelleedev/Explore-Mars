e3d9929387f8987d7d5138731f35d8d4471647af
unknown <michaellee0209@gmail.com>
2023-06-10 16:32:08 -0700

    changed navbar collapse from offcanvas to collapse

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -10,7 +10,7 @@ export default function NavbarComponent() {
     return (
         <div>
             <>
-                <Navbar bg="dark" expand="md">
+                <Navbar collapseOnSelect variant="dark" bg="dark" expand="md">
                     <Container fluid>
                         <Navbar.Brand href={homeURL}>
                             <img
@@ -18,30 +18,16 @@ export default function NavbarComponent() {
                                 width="30px"
                                 height="30px"
                                 className="d-inline-block align-top"
-                                alt="logo"
-                            />
+                                alt="logo"/>
                         </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="offcanvasNavbar-expand-sm" className="bg-secondary bg-opacity-50" />
-
-                        <Navbar.Offcanvas
-                            id="offcanvasNavbar-expand-sm"
-                            className="bg-dark"
-                            aria-labelledby="offcanvasNavbarLabel-expand-sm"
-                            placement="end"
-                        >
-                            <Offcanvas.Header closeButton>
-                                <Offcanvas.Title id="offcanvasNavbarLabel-expand-sm" className="text-light">
-                                    Menu
-                                </Offcanvas.Title>
-                            </Offcanvas.Header>
-                            <Offcanvas.Body>
-                                <Nav className="justify-content-end flex-grow-1 pe-3">
-                                    <Nav.Link href="#" className="text-light" >About</Nav.Link>
-                                    <Nav.Link href="#" className="text-light" >Things To Do</Nav.Link>
-                                    <Nav.Link href="#" className="text-light" >Travel Information</Nav.Link>
-                                </Nav>
-                            </Offcanvas.Body>
-                        </Navbar.Offcanvas>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="justify-content-end flex-grow-1 pe-auto">
+                                <Nav.Link href="#" className="text-grey" >About</Nav.Link>
+                                <Nav.Link href="#" className="text-grey" >Things To Do</Nav.Link>
+                                <Nav.Link href="#" className="text-grey" >Travel Information</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
                     </Container>
                 </Navbar>
             </>
