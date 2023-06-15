3780e70fa8073ff9c9cf9c365457ed995380f99e
unknown <michael.gunhyo@gmail.com>
2023-06-14 18:17:13 -0700

    updated navbar look

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav'
+import Nav from 'react-bootstrap/Nav';
+
+import '../styles/navbar.css';
 import logo from '../images/logo.png';
+
 
 export default function NavbarComponent() {
     let homeURL = "/"
@@ -17,14 +20,14 @@ export default function NavbarComponent() {
                                 width="30px"
                                 height="30px"
                                 className="d-inline-block align-top"
-                                alt="logo"/>
+                                alt="logo" />
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav">
-                            <Nav className="justify-content-end flex-grow-1 pe-auto">
-                                <Nav.Link href="#" className="text-grey" >About</Nav.Link>
-                                <Nav.Link href="#" className="text-grey" >Things To Do</Nav.Link>
-                                <Nav.Link href="#" className="text-grey" >Travel Information</Nav.Link>
+                            <Nav className="justify-content-md-center flex-grow-1 pe-auto">
+                                <Nav.Link href="#" >What To Expect</Nav.Link>
+                                <Nav.Link href="#" >Things To Do</Nav.Link>
+                                <Nav.Link href="#" >Travel Information</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
