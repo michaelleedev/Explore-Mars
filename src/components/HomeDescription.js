d511f633a87fb583cc7099064d7d2fcbc286d5f6
unknown <michael.gunhyo@gmail.com>
2023-06-15 17:25:54 -0700

    added basic info section on home page

---
+++ src/components/HomeDescription.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+import Item1 from "../images/DescriptionItem1.jpg";
+
+import '../styles/description.css'
+
+export default function HomeDescription() {
+
+
+    return (
+        <div>
+            <Container fluid className="description-container">
+                <h1 className="section-title">EXPLORE MARS</h1>
+                <Row>
+                    <Col className="col-md-4 col-12">
+                        <div className="description-item">
+                            <h1>Basic Information</h1>
+                            <h3>Traveling to Mars is a bit different from traveling on Earth, and we got you covered</h3>
+                        </div>
+
+                        <Button href="#" variant="outline-secondary" size="lg" className="mt-5">Lean More</Button>
+                    </Col>
+                    <Col className="col-md-4 col-12">
+                        <div className="description-item">
+                            <h1>Scenaries and Activities</h1>
+                            <h3>Experience what it's like to live on Mars with various activities to do and scenaries to explore</h3>
+                        </div>
+
+                        <Button href="#" variant="outline-secondary" size="lg" className="mt-5">Lean More</Button>
+                    </Col>
+                    <Col className="col-md-4 col-12">
+                        <div className="description-item">
+                            <h1>Customizable Trave Plans</h1>
+                            <h3>Start with our basic itinerary and customize it however you want</h3>
+                        </div>
+                        <Button href="#" variant="outline-secondary" size="lg" className="mt-5">Lean More</Button>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
